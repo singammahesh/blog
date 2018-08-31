a39e062a0d1cd0e6b4e95693cfb329ced1263003
mahesh reddy <singammaheshreddy167@gmail.com>
2018-08-31 01:09:01 -0700

    created models in jobsapp

--- jobs/models.py
+++ jobs/models.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class job(models.Model):
+    image=models.ImageField(Upload_to='images/')
+    summary=models.CharField(max_length=200)
