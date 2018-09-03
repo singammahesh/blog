02d6fca6d4f9a90a2437806a6063455fa230c5f9
mahesh reddy <singammaheshreddy167@gmail.com>
2018-09-02 23:08:19 -0700

    my first commit

--- jobs/models.py
+++ jobs/models.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 class job(models.Model):
-    image=models.ImageField(Upload_to='images/')
+    image=models.ImageField(upload_to='images/')
     summary=models.CharField(max_length=200)
